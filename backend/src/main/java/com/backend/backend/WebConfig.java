348c570f29ee244d9448edb9409c6aebb6f19452
Genisson Emilio <esgenisson@gmail.com>
2025-04-29 09:21:28 -0300

    update webconfig

---
+++ backend/src/main/java/com/backend/backend/WebConfig.java
@@ -0,0 +1,18 @@
+package com.backend.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permite todas as rotas
+                .allowedOrigins("http://localhost:3000") // Frontend React (ajuste conforme necessário)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Métodos permitidos
+                .allowedHeaders("*"); // Permite todos os cabeçalhos
+    }
+}
