6f069c5cfd50c02889679e0808e2becb1653de1b
Genisson Emilio <esgenisson@gmail.com>
2025-05-01 20:01:54 -0300

    config backend

---
+++ backend/src/main/java/com/backend/backend/dto/RegisterDTO.java
@@ -0,0 +1,7 @@
+package com.backend.backend.dto;
+
+public class RegisterDTO {
+    public String nome;
+    public String email;
+    public String senha;
+}
---
+++ backend/src/main/java/com/backend/backend/dto/TokenResponse.java
@@ -0,0 +1,10 @@
+package com.backend.backend.dto;
+
+class TokenResponse {
+    public String token;
+
+    public TokenResponse(String token) {
+        this.token = token;
+    }
+}
+
